710fd1465e8a10520de9aef9bb876258d99ac823
Najad Nabil <najadnabil2612@gmail.com>
2026-05-30 17:08:27 +0530

    Added 'View All Books' feature

--- main_using_oop.py
+++ main_using_oop.py
@@ -43,12 +43,27 @@ class Library:
         self.books.append(book)
         print(f"{book.book_title} has been added successfully with ID {self.next_id}")
         self.next_id += 1
+
+    def view_all_books(self):
+       for index , book in enumerate(self.books , start = 1):
+            print(f"""Book No : {index}
+Book Name : {book.book_title}
+Author : {book.book_author}
+Book ID : {book.book_id}""")
+            if book.is_available:
+                print("Availablity : Currently available")
+            else:
+                print("Availablity : Out of Stock")
+            print("-" * 30)
+
+        
+
     
 
 library = Library()
 library.add_book()
 library.add_book()
-print(library.books)
+library.view_all_books()
 
 
 
